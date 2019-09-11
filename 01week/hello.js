d4670973f524fc7f69585026d07afd128e0dbea1
tonezone108 <tonezone108@gmail.com>
2019-09-10 20:19:11 -0500

    hello world file

---
+++ 01week/hello.js
@@ -0,0 +1,9 @@
+console.log(
+  "Hello World... Programed to work and not to feel... Not even sure if this is real..."
+);
+
+var color = "red";
+var othercolor = "blue";
+
+console.log("my color is ", color);
+console.log("my other color is ", othercolor);
